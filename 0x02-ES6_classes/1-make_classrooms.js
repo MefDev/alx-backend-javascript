40a3ca3bdab76e56a121fb92f749a79ce2558724
Mefdev <mefdev.pro@gmail.com>
2024-03-07 23:47:45 +0100

    Make a classroom of the class

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms(){
+    const firstRoom = new ClassRoom(19);
+    const secondRoom = new ClassRoom(20);
+    const thirdRoom = new ClassRoom(34);
+    return [firstRoom, secondRoom, thirdRoom];
+}
