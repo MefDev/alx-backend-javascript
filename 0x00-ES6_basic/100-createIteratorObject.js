8e44228516105eed7e57acd4bba423b29967b2b9
Mefdev <mefdev.pro@gmail.com>
2024-03-08 22:14:19 +0100

    pushing all files

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   const employeesList = report.allEmployees;
   const allEmployees = [];
   for (const department in employeesList) {
-    allEmployees.push(...employeesList[department]);
+    if (employeesList[department]) allEmployees.push(...employeesList[department]);
   }
   return allEmployees;
 }
