30a74d9db8a4d514c47f545d25cd7e1e74a82832
Mefdev <mefdev.pro@gmail.com>
2024-03-08 22:17:43 +0100

    Iterate Using Join

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const allEmployees = [];
-  for (const department in reportWithIterator) {
-    allEmployees.push(...reportWithIterator[department]);
-  }
-  return allEmployees.join(' | ');
+    return reportWithIterator.join(' | ');
 }
