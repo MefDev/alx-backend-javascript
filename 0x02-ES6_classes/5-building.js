249f57d970cff8f4c30851f37de42c7b15bf53bd
Mefdev <mefdev.pro@gmail.com>
2024-03-09 23:41:33 +0100

    pushing all files

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor != Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
-    this._sqft;
-  }
-
-  set sqft(value) {
-    this._sqft = value;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    return this._sqft;
   }
 }
