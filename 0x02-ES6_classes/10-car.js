a341391e348ad3203698811824f8606999c8245b
Mefdev <mefdev.pro@gmail.com>
2024-03-10 01:44:34 +0100

    pushing all files

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,19 +5,7 @@ export default class Car {
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  get color() {
-    return this.color;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
   cloneCar() {
-    return new Car();
+    return new this.constructor();
   }
 }
